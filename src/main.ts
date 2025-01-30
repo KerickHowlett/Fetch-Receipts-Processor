2d5b7bfc34625d969892aa58708e366645bfe46a
Kerick Howlett <8661181+KerickHowlett@users.noreply.github.com>
2025-01-30 14:04:51 -0500

    style(core): apply previx const

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,22 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
     const GLOBAL_PREFIX = 'api' as const;
-    app.setGlobalPrefix('api');
+    app.setGlobalPrefix(GLOBAL_PREFIX);
+
+    const apiDocsConfig = new DocumentBuilder()
+        .setTitle('Receipt Processor')
+        .setDescription('A simple receipt processor')
+        .setVersion('1.0.0')
+        .build();
+    const documentFactory = () => SwaggerModule.createDocument(app, apiDocsConfig);
+    SwaggerModule.setup(GLOBAL_PREFIX, app, documentFactory);
 
     const port = +process.env.PORT || 3000;
     await app.listen(port);
