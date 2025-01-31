a1549ca855d2c42e5194366bb791267d86f1f00f
Kerick Howlett <8661181+KerickHowlett@users.noreply.github.com>
2025-01-30 19:19:14 -0500

    refactor(receipts): rename consts file

--- src/receipts/constants/receipts.const.ts
+++ src/receipts/constants/receipts.const.ts
@@ -1,3 +1,7 @@
+// DEFAULT POINT SCORES
+export const NO_POINTS = 0;
+
+// ERROR MESSAGES
 export const INVALID_RECEIPT = 'The receipt is invalid.' as const;
 export const RECEIPT_EXISTS = 'A receipt with that ID already exists.' as const;
 export const RECEIPT_NOT_FOUND = 'No receipt found for that ID.' as const;
